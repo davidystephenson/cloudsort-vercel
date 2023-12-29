c7220bb6eeb0da0e3b333ff84098d7ab0bae5b53
David Y. Stephenson <david@davidystephenson.com>
2023-12-28 21:00:36 -0500

    Intercept control-f search

--- lib/table/table-search-view.tsx
+++ lib/table/table-search-view.tsx
@@ -1,13 +1,19 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useRef, useState } from 'react'
 import ThemeInputView from '../theme/theme-input-view'
 import { useTable } from './table-context'
 import ThemeButtonView from '../theme/theme-button-view'
 import { MdClear } from 'react-icons/md'
 import ThemeIconView from '../theme/theme-icon-view'
+import { useHotkeys } from 'react-hotkeys-hook'
 
 export default function TableSearchView (): JSX.Element {
   const table = useTable()
   const [query, setQuery] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
+  useHotkeys('ctrl+f', () => {
+    event?.preventDefault()
+    inputRef.current?.focus()
+  })
   function handleQueryChange (event: ChangeEvent<HTMLInputElement>): void {
     setQuery(event.target.value)
     table.filterRows({ query: event.target.value })
@@ -36,6 +42,7 @@ export default function TableSearchView (): JSX.Element {
         label='Search'
         onChange={handleQueryChange}
         value={query}
+        ref={inputRef}
       />
     </>
   )
