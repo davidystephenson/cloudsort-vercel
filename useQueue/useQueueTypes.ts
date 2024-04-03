6e5c13b4d4a93af60c0d5c5b1c18523ff29f238e
David Y. Stephenson <david@davidystephenson.com>
2024-04-02 21:23:34 -0400

    Simplify queue types

--- useQueue/useQueueTypes.ts
+++ useQueue/useQueueTypes.ts
@@ -5,23 +5,17 @@ export interface Task<Result> {
   label: string
 }
 interface AddMessage {
-  error?: undefined
-  result?: undefined
   status: 'add'
 }
 interface StartMessage {
-  error?: undefined
-  result?: undefined
   status: 'start'
 }
 interface CompleteMessage <Result> {
-  error?: undefined
   result: Result
   status: 'complete'
 }
 interface ErrorMessage {
   error: Error
-  result?: undefined
   status: 'error'
 }
 type StatusMessage <Result> = AddMessage | StartMessage | CompleteMessage<Result> | ErrorMessage
